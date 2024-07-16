0fe0555c7f2d150fc55e40b20e54102c3d275fa5
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-07-16 15:22:50 +0200

    Erase components from app.ts to merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TicketTableComponent } from './Components/ticket-table/ticket-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TicketTableComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
