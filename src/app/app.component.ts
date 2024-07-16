152e267bc729e1d89412af8724cd62df116c1b7a
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-07-16 15:19:42 +0200

    Add ticket table with Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TicketTableComponent } from './Components/ticket-table/ticket-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TicketTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'super-ticketing';
