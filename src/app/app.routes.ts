032279eaca314d5d7ad2417b9cf28c5e10c87bd1
MegaDraconius <ivanvclinetor@gmail.com>
2024-07-17 10:37:41 +0200

    refactor: Add the router outlet and the routes of the pages user and admin confirm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AdminConfirmationPageComponent } from './Pages/admin-confirmation-page/admin-confirmation-page.component';
+import { UserConfirmationPageComponent } from './Pages/user-confirmation-page/user-confirmation-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'adminConfirmation', component: AdminConfirmationPageComponent },
+  { path: 'userConfirmation', component: UserConfirmationPageComponent },
+];
