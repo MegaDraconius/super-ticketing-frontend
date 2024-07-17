4fd2d4f8c03843b01d06582911c97d8ddb012dec
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2024-07-17 14:54:29 +0200

    feat Create Tickets form component and funtions

---
+++ src/app/Components/ticketsForm/ticket-form/ticket-form.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup,ReactiveFormsModule, Validators } from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
+import { TicketForm } from '../../../Shared/Interfaces/ticket-form';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-ticket-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink, MatRadioModule, MatButtonModule],
+  templateUrl: './ticket-form.component.html',
+  styleUrl: './ticket-form.component.scss'
+})
+export class TicketFormComponent {
+  @Input() ticketsForm!: FormGroup;
+  @Output() sendForm = new EventEmitter<TicketForm>();
+  technicalProblem: boolean = false;
+
+  // constructor(private fb: FormBuilder) {
+  //   this.ticketsForm = this.fb.group({
+  //     title: ['', Validators.required],
+  //     details: ['', Validators.required],
+  //   });
+  // }
+
+  onMotiveClick(value: string) {
+    if (value == '2') {
+      console.log("TRUE")
+      this.technicalProblem = true;
+      console.log(this.technicalProblem)
+      
+      
+    } else {
+      console.log("FALSE")
+      this.technicalProblem = false;
+    }
+
+  }
+
+
+  submit(){
+    const ticket:TicketForm={
+      title: this.ticketsForm.controls["title"].value,
+      details:this.ticketsForm.controls["details"].value,
+      img.this.ticketsForm.controls["imagen"].File,
+      id:0
+  }
+  this.sendForm.emit(ticket);
+  this.sendForm.router.navegator('/userConfirmation');
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormPageComponent } from "./Pages/form-page/form-page.component";
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormPageComponent, MatRadioModule,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
