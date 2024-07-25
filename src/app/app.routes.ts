f393cc4b77b984cd69b30a30b0e2e3a5e5d03f02
MegaDraconius <ivanvclinetor@gmail.com>
2024-07-25 11:37:14 +0200

    refactor: change routes of tables components and add personal assignment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,13 @@
 import { Routes } from '@angular/router';
 import { EmptyTicketComponent } from './Components/empty-ticket/empty-ticket.component';
 import { LayoutComponent } from './Components/layout/layout/layout.component';
-import { TicketTableComponent } from './Components/ticket-table/ticket-table.component';
+import { AdminTableComponent } from './Components/Table/admin-table/admin-table.component';
+import { TicketTableComponent } from './Components/Table/user-table/ticket-table.component';
 import { TicketFormComponent } from './Components/ticketsForm/ticket-form/ticket-form.component';
 import { AdminConfirmationPageComponent } from './Pages/admin-confirmation-page/admin-confirmation-page.component';
 import { LoginComponent } from './Pages/login/login.component';
-import { UserConfirmationPageComponent } from './Pages/user-confirmation-page/user-confirmation-page.component';
-import { TicketFormComponent } from './Components/ticketsForm/ticket-form/ticket-form.component';
-import { AdminTableComponent } from './Components/Table/admin-table/admin-table.component';
 import { PersonalAssignmentComponent } from './Pages/personal-assignment/personal-assignment.component';
-';
-
+import { UserConfirmationPageComponent } from './Pages/user-confirmation-page/user-confirmation-page.component';
 
 export const routes: Routes = [
   {
@@ -18,18 +15,16 @@ export const routes: Routes = [
     component: LayoutComponent,
     children: [
       { path: '', component: EmptyTicketComponent },
-      { path: 'table', component: TicketTableComponent },
       { path: 'form', component: TicketFormComponent },
       { path: 'adminConfirmation', component: AdminConfirmationPageComponent },
-      { path: 'userConfirmation', component: UserConfirmationPageComponent }, 
-      {path: 'adminTable', component: AdminTableComponent},
-      { path: 'detailedView', component:PersonalAssignmentComponent },
-
+      { path: 'userConfirmation', component: UserConfirmationPageComponent },
+      { path: 'adminTable', component: AdminTableComponent },
+      { path: 'userTable', component: TicketTableComponent },
+      { path: 'detailedView', component: PersonalAssignmentComponent },
     ],
   },
   { path: 'layout', redirectTo: '' },
   { path: 'login', component: LoginComponent },
   { path: 'adminConfirmation', component: AdminConfirmationPageComponent },
   { path: 'userConfirmation', component: UserConfirmationPageComponent },
-  
 ];
