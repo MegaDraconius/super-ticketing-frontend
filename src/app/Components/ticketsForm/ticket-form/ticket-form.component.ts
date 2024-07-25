3ea7b2f334f8dd889333cf40986aaf2ee3403104
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-07-25 15:55:37 +0200

    modify post method

--- src/app/Components/ticketsForm/ticket-form/ticket-form.component.ts
+++ src/app/Components/ticketsForm/ticket-form/ticket-form.component.ts
@@ -1,41 +1,55 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup,ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatRadioModule} from '@angular/material/radio';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 import { TicketForm } from '../../../Shared/Interfaces/ticket-form';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { TicketServiceService } from '../../../Services/ticket-service.service';
+import { Ticket } from '../../../Shared/ticket';
 
 @Component({
   selector: 'app-ticket-form',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, RouterOutlet,MatRadioModule, MatButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    RouterOutlet,
+    MatRadioModule,
+    MatButtonModule,
+  ],
   templateUrl: './ticket-form.component.html',
-  styleUrl: './ticket-form.component.scss'
+  styleUrl: './ticket-form.component.scss',
 })
 export class TicketFormComponent {
   ticketsForm!: FormGroup;
   @Output() sendForm = new EventEmitter<TicketForm>();
   technicalProblem: boolean = false;
-  
-  router = inject(Router)
-  
+
+  router = inject(Router);
+
+  ticketService = inject(TicketServiceService);
+
   constructor(private fb: FormBuilder) {
     this.ticketsForm = fb.group({
       title: ['', Validators.required],
       details: ['', Validators.minLength(3)],
-      img:[null],
+      img: [null],
       motive: ['', Validators.required],
     });
   }
-  
+
   onMotiveClick(value: string) {
     if (value == '2') {
-      console.log("TRUE")
+      console.log('TRUE');
       this.technicalProblem = true;
-      console.log(this.technicalProblem)
-      
+      console.log(this.technicalProblem);
     } else {
-      console.log("FALSE")
+      console.log('FALSE');
       this.technicalProblem = false;
     }
   }
@@ -43,22 +57,38 @@ export class TicketFormComponent {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
       this.ticketsForm.patchValue({
-        img: file
+        img: file,
       });
     }
   }
-  submit(){
-    if(this.ticketsForm.valid){
-      const ticket:TicketForm={
-        title: this.ticketsForm.controls["title"].value,
-        details:this.ticketsForm.controls["details"].value,
-        img:this.ticketsForm.controls["img"].value,
-      } 
-      this.router.navigate(["/userConfirmation"]);
+  submit() {
+    if (this.ticketsForm.valid) {
+      // const ticket:TicketForm={
+      //   title: this.ticketsForm.controls["title"].value,
+      //   details:this.ticketsForm.controls["details"].value,
+      //   img:this.ticketsForm.controls["img"].value,
+      // }
+
+      const ticket: Ticket = {
+        TrackingId: 'ES-00000000003',
+        Title: this.ticketsForm.controls['title'].value,
+        Description: this.ticketsForm.controls['details'].value,
+        ReportDate: '2024-07-24T07:33:18.165Z',
+        // SolvedDate: '2024-07-24T07:33:18.165Z',
+        Status: 'Pending',
+        Country: '669a13bee4cd3cf42f7728db',
+        Priority: '8',
+        // Image: this.ticketsForm.controls['img'].value,
+        Photo: 'photo test',
+        UserId: '66a20b4c2b51e2b2d11e22d1',
+      };
+
+      console.log(ticket);
+      this.ticketService.createTicket(ticket);
+
+      this.router.navigate(['/userConfirmation']);
+    } else {
+      alert('debe llenar todos los campos');
     }
-    else{
-      alert('debe llenar todos los campos')
-    }
-    
   }
 }
--- src/app/Services/ticket-service.service.ts
+++ src/app/Services/ticket-service.service.ts
@@ -1,18 +1,40 @@
 import { Injectable, signal } from '@angular/core';
 import { TicketForm } from '../Shared/Interfaces/ticket-form';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { catchError, firstValueFrom, of, tap } from 'rxjs';
 import { Ticket } from '../Shared/ticket';
 import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TicketServiceService {
   backUrl = environment.API_URL;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTickets() {
-    return this.http.get<Ticket[]>(this.backUrl.concat("/api/Ticket")).pipe(tap(x => {return x}));
+    return this.http.get<Ticket[]>(this.backUrl.concat('/api/Ticket')).pipe(
+      tap((x) => {
+        return x;
+      })
+    );
+  }
+
+  async createTicket(ticket: Ticket) {
+    console.log('Ticket: ', ticket);
+    console.log('URL: ', this.backUrl.concat('/api/Ticket'));
+
+    // return this.http.post<Ticket>(this.backUrl.concat('/api/Ticket'), ticket);
+
+    try {
+      const result = await firstValueFrom(
+        this.http
+          .post<Ticket>(this.backUrl.concat('/api/Ticket'), ticket)
+          .pipe(catchError((e) => of(e)))
+      );
+      return result;
+    } catch (e) {
+      console.error(e);
+    }
   }
 }
