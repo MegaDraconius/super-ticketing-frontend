93f20fc1849d88b6b45d07b820362d3087026fa5
MegaDraconius <ivanvclinetor@gmail.com>
2024-07-30 10:53:50 +0200

    refactor: Add a new propierty on tickets

--- src/app/Shared/Interfaces/admin-ticket.ts
+++ src/app/Shared/Interfaces/admin-ticket.ts
@@ -1,8 +1,9 @@
 export interface AdminTicket {
-    Id: string,
-    ticketTitle: string,
-    user:string,
-    startDate: string,
-    priority:string,
-    status: string,
+  Id: string;
+  ticketTitle: string;
+  user: string;
+  startDate: string;
+  priority: string;
+  status: string;
+  archived?: boolean;
 }
