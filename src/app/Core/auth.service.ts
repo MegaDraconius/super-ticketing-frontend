5a91ff02d365e8d31934cdf5742bba3d1e2ea9c1
alexmrtc <alexmrtc@gmail.com>
2024-07-30 19:53:23 +0200

    feat: create login request to server and refactor interface

--- src/app/Core/auth.service.ts
+++ src/app/Core/auth.service.ts
@@ -2,33 +2,32 @@
 import { inject, Injectable } from '@angular/core';
 import { User } from '../Shared/Interfaces/user';
 import { UserResponse } from '../Shared/Interfaces/user-response';
+import { catchError, firstValueFrom, of } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  // private http = inject(HttpClient); // manejar las peticiones de la API (con el get())
-  // private url = environment.apiUrl;
+  private http = inject(HttpClient); // manejar las peticiones de la API (con el get())
+  private backUrl = environment.API_URL;
 
   async login(user: User) { // el async require del await posteriormente
     try {
-      //Implementar en un futuro cuando tengamos el backend
-      // const result = await firstValueFrom(this.http.post<User>(this.url.concat("/login"), user).pipe(catchError // FirstValueFrom(rxjs) convierte el observable en promesa al suscribirse en el observable - hay una promesa con el asyinc/await
-      //   (e => of(e))))
-
-      const result : UserResponse = {
-        accessToken: "asukdasjdasjgdhjgdjgasdhagsshjdgajgad1762",
-        user: {
-          email: "a@a.com",
-          id: 1,
-          isAdmin: true
-        }
-      }
-      console.log(result)
+      const result = await firstValueFrom(this.http.post<User>(this.backUrl.concat("/api/Login"), user).pipe(catchError
+        (e => of(e))))
 
       return result;
     } catch (e) {
       throw e
     }
+  }
+
+  async sha256(stringToEncrypt: string) {
+    const arrayBuffer = new TextEncoder().encode(stringToEncrypt)
+    const hashAsArrayBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
+    const uint8ViewOfHash = new Uint8Array(hashAsArrayBuffer);
+    return Array.from(uint8ViewOfHash).map((b) => b.toString(16).padStart(2, '0')).join('');
   }
 }
